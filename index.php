40232f1e73ff76bbcb23b448e8d4d15eee51ebe9
Leatpt <lea.kibur@tptlive.ee>
2024-04-28 16:17:26 +0300

    Lesson 8 - Functions and Filters

--- index.php
+++ index.php
@@ -14,32 +14,50 @@
             [
                 'name' => 'Do Androids Dream of Electric Sheep', 
                 'author' => 'Philip K. Dick',
-                'releaseYear' => '1968',
+                'releaseYear' => 1968,
                 'purchaseUrl' => 'http://example.com'
             ],
             [
                 'name' => 'The Langoliers', 
                 'author' => 'Stephen King',
-                'releaseYear' => '1990',
+                'releaseYear' => 1990,
                 'purchaseUrl' => 'http://example.com'
             ],
             [
                 'name' => 'Hail Mary', 
                 'author' => 'Andy Weir',
-                'releaseYear' => '2021',
+                'releaseYear' => 2021,
+                'purchaseUrl' => 'http://example.com'
+            ],
+            [
+                'name' => 'The Martian', 
+                'author' => 'Andy Weir',
+                'releaseYear' => 2011,
                 'purchaseUrl' => 'http://example.com'
             ],
         ];
+
+        function filterByAuthor($books, $author){
+            $filteredBooks = [];
+
+            foreach ($books as $book){
+                if($book['author'] === $author){
+                    $filteredBooks[] = $book;
+                }
+            }
+
+            return $filteredBooks;
+        }
     ?>
 
     <ul>
-        <?php foreach ($books as $book) : ?>
-            <li>
-                <a href ="<?=$book['purchaseUrl']; ?>">
-                    <?= $book['name']; ?> (<?= $book['releaseYear']; ?>)
-                </a>
-            </li>
-        <?php endforeach ?>
+        <?php foreach (filterByAuthor($books, 'Stephen King') as $book) : ?>
+                <li>
+                    <a href ="<?=$book['purchaseUrl']; ?>">
+                        <?= $book['name']; ?> (<?= $book['releaseYear']; ?>) - By <?= $book['author']; ?>
+                    </a>
+                </li>
+        <?php endforeach; ?>
     </ul>
     
 </body>
