3416444c09ab7f1c3a17deb3d3377cef0fc1de3f
Leatpt <lea.kibur@tptlive.ee>
2024-04-28 15:26:19 +0300

    Lesson 5 - Conditionals and Booleans

--- index.php
+++ index.php
@@ -4,15 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 <body>
+    <?php 
+        $bookName = "Dark Matter";
+        $read = true;
+
+        if ($read) {
+            $message = "You have read \"$bookName\"";
+        } else {
+            $message = "You have NOT read \"$bookName\"";
+        }
+    ?>
 
     <h1>
-        <?php
-            $greeting = "Hello";
-            $subject = "Universe";
-            echo "$greeting $subject"
-        ?>
+        <?= $message; ?>
     </h1>
 
 </body>
