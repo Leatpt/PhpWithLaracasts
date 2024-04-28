0b444ccfb9cca0ced392c875b85b2657c2126b7b
Leatpt <lea.kibur@tptlive.ee>
2024-04-28 15:58:53 +0300

    Lesson 7 - Associative Arrays

--- index.php
+++ index.php
@@ -11,16 +11,35 @@
 
     <?php 
         $books = [
-            "Do Androids Dream of Electric Sheep",
-            "The Langoliers",
-            "Hail Mary",
+            [
+                'name' => 'Do Androids Dream of Electric Sheep', 
+                'author' => 'Philip K. Dick',
+                'releaseYear' => '1968',
+                'purchaseUrl' => 'http://example.com'
+            ],
+            [
+                'name' => 'The Langoliers', 
+                'author' => 'Stephen King',
+                'releaseYear' => '1990',
+                'purchaseUrl' => 'http://example.com'
+            ],
+            [
+                'name' => 'Hail Mary', 
+                'author' => 'Andy Weir',
+                'releaseYear' => '2021',
+                'purchaseUrl' => 'http://example.com'
+            ],
         ];
     ?>
 
     <ul>
         <?php foreach ($books as $book) : ?>
-            <li><?= $book ?></li>
-        <?php endforeach; ?>
+            <li>
+                <a href ="<?=$book['purchaseUrl']; ?>">
+                    <?= $book['name']; ?> (<?= $book['releaseYear']; ?>)
+                </a>
+            </li>
+        <?php endforeach ?>
     </ul>
     
 </body>
