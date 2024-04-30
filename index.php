00d2727a5a1c843576ff6cb2e18d5a336f8b2b19
Leatpt <lea.kibur@tptlive.ee>
2024-04-30 20:40:10 +0300

    Lesson 17 - PDO FIrst Steps

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+//require 'router.php';
+
+//connect to MySQL database.
+
+$dsn = "mysql:host=127.0.0.1;dbname=demo;port=3306;user=root;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("SELECT * FROM myapp WHERE id = 1");
+$statement->execute();
+
+$myapp = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($myapp as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
