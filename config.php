cb6c8386022633177a8966c524aaf030ff6b8540
Leatpt <lea.kibur@tptlive.ee>
2024-05-01 13:54:27 +0300

    Lesson 20 - SQL Injection Vulnerabilities

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 return [
     'database' => [
-        'host' => "127.0.0.1",
+        'host' => 'localhost',
         'port' => 3306,
         'dbname' => 'demo',
         'charset' => "utf8mb4"
--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <?php
 
 require 'functions.php';
-//whenever you have a .php file that only contains a class, it starts with a Capital
+// whenever you have a .php file that only contains a class, it starts with a Capital
 require 'Database.php'; 
-//require 'router.php';
+// require 'router.php';
 
 $config = require('config.php');
+$db = new Database($config['database']);
 
-$db = new Database($config);
-$myapp = $db->query("select * from myapp")->fetchAll();
+$id = ($_GET['id']);
+// never inline user data into a query string 
+// "select * from myapp where id = {$id}", either use ? or key-d wildcard parameter
+$query = "select * from myapp where id = :id";
+
+
+$myapp = $db->query($query, [':id' => $id])->fetch();
 
 dd($myapp);
