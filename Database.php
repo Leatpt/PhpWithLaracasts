8f9fe536d30e403c2742ff07c146394d61737a36
Leatpt <lea.kibur@tptlive.ee>
2024-05-01 13:18:05 +0300

    Lesson 19 - Environments and Configuration

--- Database.php
+++ Database.php
@@ -2,11 +2,15 @@
 
 class Database {
     public $connection;
-    public function __construct()
+    public function __construct($config, $username = 'root', $password = '')
     {
-        $dsn = "mysql:host=127.0.0.1;dbname=demo;port=3306;user=root;charset=utf8mb4";
+        $dsn = ('mysql:' . http_build_query($config, '', ';')); 
+        // I'm using the http_build_query to get the same result but shorter
+        // $dsn = "mysql:host={$config['host']};dbname={$config['dbname']};port={$config['port']};charset={$config['charset']}";
         
-        $this->connection = new PDO($dsn);
+        $this->connection = new PDO($dsn, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
     }
     public function query($query)
     {
--- index.php
+++ index.php
@@ -3,12 +3,11 @@
 require 'functions.php';
 //whenever you have a .php file that only contains a class, it starts with a Capital
 require 'Database.php'; 
-
-
 //require 'router.php';
 
+$config = require('config.php');
 
-$db = new Database();
-$myapp = $db->query("select * from myapp")->fetchAll(PDO::FETCH_ASSOC);
+$db = new Database($config);
+$myapp = $db->query("select * from myapp")->fetchAll();
 
 dd($myapp);
